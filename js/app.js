f42fbf6c3f8c0541a7ab714c59325b292561d736
Valentina Iramain <valentinairamain2020@gmail.com>
2025-10-14 11:06:25 -0300

    se agrega control para que no pueda ingresar una tarea vacía

--- js/app.js
+++ js/app.js
@@ -2,34 +2,33 @@ function cargarTarea(e) {
     e.preventDefault();
     const inputTarea = document.querySelector('input')
     console.log(inputTarea.value)
+    if (inputTarea.value){
 
+        // Crear li con create element
+        const li = document.createElement('li');
+        li.className = 'list-group-item d-flex justify-content-between align-items-center';
+        li.textContent = inputTarea.value;
 
-    // Crear li con create element
-    const li = document.createElement('li');
-    li.className = 'list-group-item d-flex justify-content-between align-items-center';
-    li.textContent = inputTarea.value;
+        // Crear botón con basurero
+        const btnEliminar = document.createElement('button');
+        btnEliminar.type = 'button';
+        btnEliminar.className = 'btn text-danger fs-4';
+        btnEliminar.innerHTML = '<i class="bi bi-trash3-fill"></i>';
 
-    // Crear botón con basurero
-    const btnEliminar = document.createElement('button');
-    btnEliminar.type = 'button';
-    btnEliminar.className = 'btn text-danger fs-4';
-    btnEliminar.innerHTML = '<i class="bi bi-trash3-fill"></i>';
+        // escuchar evento del botón para borrar tarea
+        btnEliminar.addEventListener('click', () => {
+            li.remove(); // elimina la tarea
+        });
 
-    // escuchar evento del botón para borrar tarea
-    btnEliminar.addEventListener('click', () => {
-        li.remove(); // elimina la tarea
-    });
+        // agregar botón al li
+        li.appendChild(btnEliminar);
+        // agregar li a la lista
+        lista.appendChild(li);
 
-    // agregar botón al li
-    li.appendChild(btnEliminar);
-    // agregar li a la lista
-    lista.appendChild(li);
-
-    formulario.reset();
-}
-
-function eliminarTarea() {
-    console.log('en eliminar tarea')
+        formulario.reset();
+    } else {
+        alert(`No se puede ingresar una tarea vacía`)
+    }
 }
 
 
